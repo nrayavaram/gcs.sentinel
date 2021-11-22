fd32453f3ad09087d2e13e9230d5c0b6b2626e47
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-22 11:20:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,32 @@
 
+provider "google" {
+ access_token = var.access_token
+ project = "airline1-sabre-wolverine"
+ #credentials = file("../key.json")
+}
+
+data "google_project_organization_policy" "policy" {
+  project    = "airline1-sabre-wolverine"
+  constraint = "constraints/storage.publicAccessPrevention"
+}
+
+resource "google_storage_bucket" "GCS" {
+  name          = "my-bucket"
+  location      = "US"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 3
+     
+    }
+    action {
+      type = "Delete"
+    }
+
+  }
+
+}
