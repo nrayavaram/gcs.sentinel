53f4ce4f2204e6164abf0a8bb0797b5f6ad5246b
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-22 11:24:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,8 @@
 provider "google" {
  access_token = var.access_token
  project = "airline1-sabre-wolverine"
- #credentials = file("../key.json")
 }
 
-data "google_project_organization_policy" "policy" {
-  project    = "airline1-sabre-wolverine"
-  constraint = "constraints/storage.publicAccessPrevention"
-}
 
 resource "google_storage_bucket" "GCS" {
   name          = "my-bucket"
