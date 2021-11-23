9c3d0ab498b15b7080b5489483d81ee51fac1e1d
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-23 18:48:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,10 +26,5 @@ resource "google_storage_bucket" "GCS" {
 
 }
 
-resource "google_storage_bucket" "example" {
- project              = "<PROJECT_ID>"
- name                 = "${local.resource_prefix}-<name>-gcsbucket"
- labels               = locals.labels
- default_kms_key_name = google_kms_crypto_key.secret.name
-}
+
 
